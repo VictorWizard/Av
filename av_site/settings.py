74adb8692864398b1d020a1632f126e1945da8c1
VictorWizard <the.witcher.mikaelson@gmail.com>
2019-12-17 22:39:30 +0600

    Changed the HTML for the site.

--- av_site/settings.py
+++ av_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q1&znnn0j&07u+g#96ocx_e8qb02vftbfc*m5tz8ktoft8#b36'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
